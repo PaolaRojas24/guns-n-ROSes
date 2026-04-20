578688397eb79ccb230895b621ff542cfb57b390
Paola <paolarojasdominguez24@gmail.com>
2026-04-20 11:25:48 -0600

    Launcher update

--- Week3/puzzlebot_sim3/launch/puzzlebot_sim_launch.py
+++ Week3/puzzlebot_sim3/launch/puzzlebot_sim_launch.py
@@ -11,6 +11,7 @@ from launch.actions import  EmitEvent, LogInfo, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.events import Shutdown
 from launch.substitutions import EnvironmentVariable
+from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
@@ -30,8 +31,39 @@ def generate_launch_description():
                             name='robot_state_publisher',
                             output='screen',
                             parameters=[{'robot_description': robot_desc}],
-                            )
+    )
 
-    l_d = LaunchDescription([robot_state_pub_node])
+    plot_node = Node(name='rqt_plot',
+                    package='rqt_plot',
+                    executable='rqt_plot',
+                    arguments=['/pose_sim/pose/position/x', '/pose_sim/pose/position/y'],
+                    )
+    
+    tree_node = Node(
+        package='rqt_gui',
+        executable='rqt_gui',
+        name='rqt_tf_tree',
+        output='screen',
+        arguments=['--standalone', 'rqt_tf_tree']
+    )
+
+    graph_node = ExecuteProcess(
+        cmd=['ros2', 'run', 'rqt_graph', 'rqt_graph'],
+        output='screen'
+    )
+
+    rviz_config = os.path.join(
+        get_package_share_directory('puzzlebot_sim3'),
+        'rviz',
+        'puzzlebot_rviz.rviz'
+    )
+
+    rviz_node = Node(name='rviz',
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config]
+    )
+
+    l_d = LaunchDescription([robot_state_pub_node, plot_node, tree_node, rviz_node, graph_node])
 
     return l_d
